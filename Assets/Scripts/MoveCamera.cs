556d290db3eab5eca982976ac1a610cc7f411221
Andree Villegas <villegasandree562@gmail.com>
2025-10-25 18:15:46 -0500

    Script para MoveCamera y posición de Layers de fondo completo

--- Assets/Scripts/MoveCamera.cs
+++ Assets/Scripts/MoveCamera.cs
@@ -3,11 +3,11 @@ using UnityEngine;
 public class MoveCamera : MonoBehaviour
 {
     [SerializeField] private float speed;
+    [SerializeField] private bool autoMove;
     void Update()
     {
-        float move = speed * Time.deltaTime;
+        float move = autoMove ? speed * Time.deltaTime : Input.GetAxis("Horizontal")*speed * Time.deltaTime;
         transform.position += new Vector3(move, 0, 0);
-
         
     }
 }
