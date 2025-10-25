92a3101a555db17c54ce0dfc497fa8986a937f86
Andree Villegas <villegasandree562@gmail.com>
2025-10-25 18:06:01 -0500

    Código de script MoveCamera

--- Assets/Scripts/MoveCamera.cs
+++ Assets/Scripts/MoveCamera.cs
@@ -2,15 +2,12 @@ using UnityEngine;
 
 public class MoveCamera : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
+    [SerializeField] private float speed;
     void Update()
     {
+        float move = speed * Time.deltaTime;
+        transform.position += new Vector3(move, 0, 0);
+
         
     }
 }
