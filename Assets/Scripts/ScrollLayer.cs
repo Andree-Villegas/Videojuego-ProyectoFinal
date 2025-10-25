638ac711f48023bd4cc5ed7834fe01512c7143fd
Andree Villegas <villegasandree562@gmail.com>
2025-10-25 18:25:20 -0500

    Código de ScrollLayer

--- Assets/Scripts/ScrollLayer.cs
+++ Assets/Scripts/ScrollLayer.cs
@@ -2,15 +2,29 @@ using UnityEngine;
 
 public class ScrollLayer : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    [SerializeField] private Transform cam; // Cámara a seguir
+    [SerializeField] private float parallaxMultiplier = 0.5f; //Velocidad del parallax
+
+    private float spriteWidth; //Ancho del fondo
+    private Vector3 lastCampos; // Última posición de la cámara
+    
+    private Transform[] backgrounds = new Transform[2];
+
     void Start()
     {
-        
+        if (transform.childCount != 2)
+        {
+            Debug.LogError($"ParallaxScrollLayer only works with 2 Bg Children, Fix '{name}' GameObject");
+            return;
+        }
+
+        //Obtiene los fondos
+        for (int i = 0; i<2; i++)
+        {
+            backgrounds[i] = transform.GetChild(i);
+        }
+
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+   
 }
