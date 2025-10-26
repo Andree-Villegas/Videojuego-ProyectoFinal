f650c93fa4b218237dd03731e0d8e019475e9e77
Andree Villegas <villegasandree562@gmail.com>
2025-10-26 01:47:44 -0500

    Primera codificación de Script PlayerController

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,17 +2,27 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-
     public float speed = 5;
+    private Rigidbody2D rb2D; //Accede al componenete de físicas en unity
+    private float move;
+    public float jumpforce = 4;
+    private bool isGrounded;
+    public Transform groundCheck;
+    public float groundRadius = 0.1f;
+    public LayerMask groundLayer;
 
     void Start()
     {
-        
+        rb2D = GetComponent <Rigidbody2D>();
     }
 
  
     void Update()
     {
-        
+        move = Input.GetAxisRaw("Horizontal");
+        rb2D.linearVelocity = new Vector2(move * speed, rb2D.linearVelocity.y);
+
+        if (move != 0)
+            transform.localScale = new Vector3(Mathf.Sign(move), 1, 1);
     }
 }
